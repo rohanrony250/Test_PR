3330d6ab4c6c1fe8041d5aa1323cddd103e79ab6
RohanRony <rohanrony@hotmail.com>
2021-02-17 16:46:35 +0300

    carousel-component

--- src/components/header-component/header-component.jsx
+++ src/components/header-component/header-component.jsx
@@ -1,13 +1,29 @@
 import React from 'react'
 import './header-styles.scss'
 import NavbarComponent from '../navbar-component/navbar-component'
+import { Container,Row,Col } from 'react-bootstrap'
+import CarouselComponent from '../carousel-component/carousel-component'
 
 
 const HeaderComponent = () =>
 (
+    
     <div className='main-header'>
         <NavbarComponent/>
+        <Container className='header-section' fluid>
+            {/* <Row>
+                <Col>
+                    <hr className='bg-primary'/>
+               </Col>
+            </Row> */}
+
+            <div className='header-carousel'>
+                <CarouselComponent/>
+            </div>
+        </Container>
     </div>
+    
+    
 )
 
 export default HeaderComponent
--- src/components/navbar-component/navbar-component.jsx
+++ src/components/navbar-component/navbar-component.jsx
@@ -6,13 +6,17 @@ const NavbarComponent = () =>
     <Container>
         <Navbar expand='lg' variant='dark'>
             <Navbar.Brand href='#' className='Navbar-brand'>VODEO</Navbar.Brand>
-            <Form inline className='mx-auto search-input'>
+            <Form inline className='search-input mx-auto'>
                 <FormControl type='text' placeholder='SEARCH' className='mr-2 search-input-field'/>
                 <Button variant='outline-light' className='search-button'><i class="fas fa-search"></i></Button>
             </Form>
+            <button className="btn btn-lg user-button">
+                <i class="fas fa-user-circle fa-lg text-light"></i>
+            
+            </button>
         </Navbar>
         <div className='navbar-titles text-center my-3'>
-            <span className='navbar-title  ml-3 p-3'>
+            <span className='navbar-title p-3'>
                 Browse
             </span>
             <span className='navbar-title p-3'>
