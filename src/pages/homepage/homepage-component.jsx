3ad71c1301a7b432ab6a8a084338b61299657e17
RohanRony <rohanrony@hotmail.com>
2021-02-18 22:46:11 +0300

    app structure

---
+++ src/pages/homepage/homepage-component.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import HeaderComponent from '../../components/header-component/header-component'
+
+
+const HomepageComponent = () =>
+(
+    <HeaderComponent/>
+)
+
+export default HomepageComponent
