5319d2b6cc305085e7c0476546ecb755f6ee973f
RohanRony <rohanrony@hotmail.com>
2021-02-19 23:11:33 +0300

    trending-topics2

--- src/components/trending-preview/trending-preview.component.jsx
+++ src/components/trending-preview/trending-preview.component.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
-
-
+import { Container,Row,Col } from 'react-bootstrap'
+import './trending-preview.styles.scss'
 const TrendingView = () =>
 (
 
-    <div>
-        hi
+    <div className='mt-5 text-center text-white'>
+        <Container>
+            <Row>
+                <Col>
+                    <h1 className='trending-topic'>
+                        TRENDING TOPICS
+                    </h1>
+                </Col>
+            </Row>
+        </Container>
     </div>
 
 )
