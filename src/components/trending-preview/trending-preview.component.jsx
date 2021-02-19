14991107b898d19d13064cf17309baf7bea37aa4
RohanRony <rohanrony@hotmail.com>
2021-02-19 23:04:15 +0300

    trending-topics1

---
+++ src/components/trending-preview/trending-preview.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const TrendingView = () =>
+(
+
+    <div>
+        hi
+    </div>
+
+)
+
+export default TrendingView
--- src/pages/homepage/homepage-component.jsx
+++ src/pages/homepage/homepage-component.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import HeaderComponent from '../../components/header-component/header-component'
+import  TrendingView  from '../../components/trending-preview/trending-preview.component'
 
 
 const HomepageComponent = () =>
 (
     <div className='homepage'>
         <HeaderComponent/>
+        <TrendingView/>
     </div>
 )
 
