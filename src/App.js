8b75620d1aeef644c39179566fcfb31d880549b4
RohanRony <rohanrony@hotmail.com>
2021-02-22 23:50:56 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import {API_KEY} from  './youtube-details/youtube-utils/youtube-utils'
 import HomepageComponent from './pages/homepage/homepage-component';
 
 
@@ -14,7 +14,7 @@ class App extends React.Component
 
   componentDidMount()
   {
-    const url = 'https://www.googleapis.com/youtube/v3'
+    const url = `https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&forUsername=GoogleDevelopers&key=${API_KEY} HTTP/1.1`
     fetch(url)
     .then(res => res.json())
       
